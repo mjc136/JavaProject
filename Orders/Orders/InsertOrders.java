d8a8706008f9420dd7576828c34fdbf32fb9d434
mjc136 <mjcullen2015@gmail.com>
2023-03-14 13:30:19 +0000

    fixed the insert order
    added inner joins and other stuff

--- Orders/Orders/InsertOrders.java
+++ Orders/Orders/InsertOrders.java
@@ -2,6 +2,8 @@ package Orders;
 
 import java.sql.*;
 
+import Users.UserSession;
+
 class InsertOrders{
 
     public static void main(String[] args) {
@@ -11,38 +13,82 @@ class InsertOrders{
         Connection connection = null ;
         PreparedStatement pstat = null;
         int orderAmount = 5;
-        int i=0;
+        UserSession.setUser(7);
+        int orderId = 0;
+        double price = 0;
+        double totalPrice = 0;
+        int itemId = 2;
      
         
         try {
             // establish connection to database
             connection = DriverManager.getConnection(DATABASE_URL, "root", "Sydpuppy2016");
             // create Prepared Statement for inserting data into table
-            String sql1 = "INSERT INTO orders(order_quantity) VALUES (?)";
-            pstat = connection.prepareStatement(sql1);
-            pstat.setInt(1, orderAmount);
-            // insert data into table
-            i = pstat.executeUpdate();
+            String sql = "INSERT INTO orders(customer_id, order_quantity, item_id) VALUES (?, ?, ?)";
+            pstat = connection.prepareStatement(sql);
+            pstat.setInt(1, UserSession.getUser()); // stores current user as customer id 
+            pstat.setInt(2, orderAmount);
+            pstat.setInt(3, itemId);
+            pstat.executeUpdate();
+            pstat.close();
 
-            // second statment
-            String sql2 = "INSERT INTO orders(customer_id, first_name, last_name, address) SELECT customer_iD, first_name, last_name, address FROM customers;";
-            i = pstat.executeUpdate(sql2);
-            // third statement
-            String sql3 = "INSERT INTO orders(item_iD, item_name) SELECT item_id, item_name FROM products;";
-            i = pstat.executeUpdate(sql3);
-            // fourth statement
-            String sql4 = "UPDATE orders SET total_price = (SELECT order_quantity * item_price FROM products WHERE orders.item_id = products.item_id)";
-            i = pstat.executeUpdate(sql4);
+            // get the order_id of the last inserted row
+            Statement stmt = connection.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT LAST_INSERT_ID()");
+            if (rs.next()) {
+            orderId = rs.getInt(1);
+            // use the orderId as needed
+            }
+            rs.close();
+            stmt.close();
 
-            System.out. println ( i + " record successfully added to the table .");
+            // update customer details for the current order
+            sql = "UPDATE orders o " +
+            "INNER JOIN customers c ON o.customer_id = c.customer_id " +
+            "SET o.first_name = c.first_name, " +
+            "o.last_name = c.last_name, " +
+            "o.address = c.address " +
+            "WHERE o.order_id = ?";
+            pstat = connection.prepareStatement(sql);
+            pstat.setInt(1, orderId); // use the order ID from the first insert statement
+            pstat.executeUpdate();
+
+            // update item details for the current order
+            sql = "UPDATE orders o " +
+            "INNER JOIN products p ON o.item_id = p.item_id " +
+            "SET o.item_name = p.item_name " +
+            "WHERE o.order_id = ?";
+            pstat = connection.prepareStatement(sql);
+            pstat.setInt(1, orderId); // use the order ID from the first insert statement
+            pstat.executeUpdate();
+
+            // get the total price of the item
+            sql = "SELECT item_price FROM products WHERE item_id=?";
+            pstat = connection.prepareStatement(sql);
+            pstat.setInt(1, itemId); // set the item ID
+            rs = pstat.executeQuery();
+            if (rs.next()) {
+                price = rs.getDouble("item_price");
+            }
+            rs.close();
+            pstat.close();
+
+            totalPrice = price * orderAmount;
+
+            // update total price for the current order
+            sql = "UPDATE orders SET total_price = ? WHERE order_id = ?";
+            pstat = connection.prepareStatement(sql);
+            pstat.setDouble(1, totalPrice);
+            pstat.setInt(2, orderId); // use the order ID from the first insert statement
+            pstat.executeUpdate();
         }
         catch(SQLException sqlException){
             sqlException . printStackTrace () ;
         }
         finally {
             try {
-                pstat.close () ;
-                connection.close () ;
+                pstat.close();
+                connection.close();
             }
             catch (Exception exception){
                 exception . printStackTrace () ;
